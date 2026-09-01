ab5abdf54a450e002c0bc4362b4b9ab101e9dfa2
Enes Kok <kokensca@gmail.com>
2026-09-01 12:14:14 +0300

    feat: add global exception handling middleware

--- src/MarketApp.API/Program.cs
+++ src/MarketApp.API/Program.cs
@@ -1,3 +1,5 @@
+using MarketApp.API.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
